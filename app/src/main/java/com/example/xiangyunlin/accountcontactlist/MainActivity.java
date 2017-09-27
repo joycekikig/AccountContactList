32d831bdbb71238f58dc828613ced13a2600d531
XiangYunLin <XiangYunLin@zeuscomm.com>
2017-09-27 17:58:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiangyunlin/accountcontactlist/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.xiangyunlin.accountcontactlist;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.Manifest.permission.WRITE_CONTACTS;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CONTACTS = 1;
+    private ListView listView;
+    
+    // Method 1
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            //未取得權限，向使用者要求允許權限
+            ActivityCompat.requestPermissions(this, new String[]{READ_CONTACTS, WRITE_CONTACTS}, REQUEST_CONTACTS);
+        }else{
+            //已有權限，可進行檔案存取
+            readContacts();
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode) {
+            case REQUEST_CONTACTS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //取得聯絡人權限，進行存取
+                    readContacts();
+                } else {
+                    //使用者拒絕權限，顯示對話框告知
+                    new AlertDialog.Builder(this)
+                            .setMessage("必須允許聯絡人權限才能顯示資料")
+                            .setPositiveButton("OK", null)
+                            .show();
+                }
+                return;
+        }
+    }
+
+    private void readContacts() {
+
+        ContentResolver resolver = getContentResolver();
+        Cursor cursor = resolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone._ID, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER},
+                null,
+                null,
+                null);
+
+
+        if(cursor.getCount() > 0) {
+            while(cursor.moveToNext()){
+                //處理每一筆資料
+                int id = cursor.getInt(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID));
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phone = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                Log.d("RECORD", id+"/"+name+"/"+phone);
+            }
+        }
+
+
+        /*
+        ListView list = (ListView) findViewById(R.id.listView);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                cursor,
+                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME},
+                new int[] {android.R.id.text1, android.R.id.text2},
+                1);
+        list.setAdapter(adapter);
+        */
+
+    }
+
+
+    /*
+    // Method 2
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    */
+}
